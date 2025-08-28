56f8b46e7dac089461af1906743cf93b0444d6e0
Felipe <felipedasilva23785@gmail.com>
2025-08-28 10:18:34 -0300

    Consertando erro de execucao do servidor local

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const router = express.Router()
 const prisma = new PrismaClient()
@@ -13,14 +13,14 @@ router.post('/cadastro', async (req, res) => {
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(user.password, salt)
 
-        const userDB = await prisma.user.create({
+        await prisma.user.create({
             data: {
                 email: user.email,
                 name: user.name,
                 password: hashPassword,
             },
         })
-        res.status(201).json(userDB)
+        res.status(201).json(user)
     }
     catch(err){
         res.status(500).json({message: "Erro no servidor, tente novamente."})
