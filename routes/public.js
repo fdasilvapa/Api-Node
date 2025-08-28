150d50ad4845b983a38d2353aa89e9c970d695c7
Felipe <felipedasilva23785@gmail.com>
2025-08-28 09:24:04 -0300

    Adicionando encriptacao de senha

--- routes/public.js
+++ routes/public.js
@@ -1,14 +1,30 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const router = express.Router()
+const prisma = new PrismaClient()
 
 //Cadastro
-router.post('/cadastro', (req, res) => {
-    const user = req.body
+router.post('/cadastro', async (req, res) => {
+    try{
+        const user = req.body
 
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(user.password, salt)
 
-
-    res.status(201).json(user)
+        const userDB = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password: hashPassword,
+            },
+        })
+        res.status(201).json(userDB)
+    }
+    catch(err){
+        res.status(500).json({message: "Erro no servidor, tente novamente."})
+    }
 })
 
 
